293605dfb14540dfbdfeb3449816cd97b5c9c287
riyak1806 <riyakanagale@gmail.com>
2026-09-24 02:23:17 +0530

    Update Supabase URL and anon key in config

--- orixa-config.js
+++ orixa-config.js
@@ -6,8 +6,8 @@
     'use strict';
 
     window.ORIXA_CONFIG = Object.assign({
-        SUPABASE_URL: 'http://127.0.0.1:54321',
-        SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwcCIsInJvbGUiOiJhb24iLCJpYXQiOjE3MDAwMDAwMDAsImV4cCI6MjAxNTA0MDAwMH0.placeholder-anon-key',
+        SUPABASE_URL: 'https://qhjpllwvjoswxbiqtqdt.supabase.co',
+        SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoanBsbHd2am9zd3hiaXF0cWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MjYxNDEsImV4cCI6MjEwNTUwMjE0MX0.xq_NQtbDvuabR6-WeuC5YTRHaMPBnh2v5W2JIlQC6uE',
         INTERNAL_AUTH_DOMAIN: 'auth.orixa.internal'
     }, window.ORIXA_CONFIG || {});
 })();
